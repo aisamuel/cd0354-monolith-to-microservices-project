efebebbb217f0ca646bbf49c1dd1aea6e347e2b5
Samuel Adeniyi <sadeniyi@afexnigeria.com>
2023-06-25 08:33:40 +0100

    updated db parameters

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -2,11 +2,11 @@ export const config = {
   'username': "samuelnewdb",
   'password': "Samuel65#*#",
   'database': "postgres",
-  'host': "samuelnewdb.ckfqkerfv4gg.us-east-1.rds.amazonaws.com",
+  'host': "samuelnewdb.cwb1fio8s3ex.us-east-1.rds.amazonaws.com",
   'dialect': 'postgres',
   'aws_region': "us-east-1",
   'aws_profile': "default",
-  'aws_media_bucket': "test-2345677-sam-dev",
+  'aws_media_bucket': "test-2345679-sam-dev",
   'url': "http://localhost:8100",
   'jwt': {
     'secret': "testing",
