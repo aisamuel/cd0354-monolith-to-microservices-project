90b30cef838ac46791aaf7614b508e1f1d613bd9
Samuel Adeniyi <sadeniyi@afexnigeria.com>
2023-06-25 21:19:20 +0100

    change url

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -7,7 +7,7 @@ export const config = {
   'aws_region': "us-east-1",
   'aws_profile': "default",
   'aws_media_bucket': "test-2345679-sam-dev",
-  'url': "http://localhost:8100",
+  'url': "http://aa9072fece3c04e70a525b3a3ca761e6-1574223951.us-east-1.elb.amazonaws.com",
   'jwt': {
     'secret': "testing",
   },
