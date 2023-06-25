2d2bc6d272890bbeab474620b016dd92d79773ad
Samuel Adeniyi <sadeniyi@afexnigeria.com>
2023-06-25 08:56:52 +0100

    feat/working

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a9e13b49998964cda866a60f7866b351-842753821.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
